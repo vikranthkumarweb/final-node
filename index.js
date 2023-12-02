1edce8f570732454c516cf4cf735d594ba926b2d
Vikranth Kumar <vikranthkumar@Vikranths-MacBook-Air.local>
2023-12-02 03:14:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http'); 
+const fs = require('fs');
+const path = require('path');
+const {MongoClient} = require('mongodb');
+
+http.createServer(async (req, res) =>{
+    console.log(req.url);
+    if(req.url ==='/') { // serve HTML
+       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content)
+       })
+    } else if(req.url ==='/style.css') { // serve CSS
+        fs.readFile(path.join(__dirname,'public','style.css'),(err,content)=>{
+             if (err) throw err ;
+             res.writeHead(200, {'Content-Type': 'text/css'});
+             res.end(content)
+        })
+     } else if(req.url ==='/script.js') { // serve JS
+        fs.readFile(path.join(__dirname,'public','script.js'),(err,content)=>{
+             if (err) throw err ;
+             res.writeHead(200, {'Content-Type': 'text/javascript'});
+             res.end(content)
+        })
+     } else if(req.url ==='/api') { // server Data
+        try {
+            const uri = "mongodb+srv://vk:vicky@cluster0.qkoht5q.mongodb.net/?retryWrites=true&w=majority";
+            const client = new MongoClient(uri);
+            try {
+                await client.connect();
+                const cursor = client.db("billionaires_db").collection("billionaires_index").find({});
+                const results = await cursor.toArray();
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(results));
+            } finally {
+                await client.close();
+            }
+        } catch(err) {
+            throw err;
+        }
+    } else {
+        res.end("<h1>404 Nothing is here</h1>")
+    }
+}).listen(9960,()=>console.log("Server is running"));
